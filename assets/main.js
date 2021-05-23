d395750502cf25c752ce853ef9bff9715f1007f3
Yuriko Goto <gotoyuriko95@gmail.com>
2021-05-23 11:50:30 +0000

    init

---
+++ assets/main.js
@@ -0,0 +1,30 @@
+// ========================== BURGEUR MENU ==========================
+function openNav() {
+  document.getElementById("myMenu").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myMenu").style.width = "0";
+}
+
+
+
+// ==========================  MODAL ==========================
+// function openModal() {
+//   document.getElementById("yuri-modal").style.display = "block";
+//   var img = document.getElementsByClassName("gallery-image");
+//   var modalImg = document.getElementById("imgShow");
+//   var captionText = document.getElementById("captionShow");
+//   console.log("テスト");
+//   modalImg.src = img.src;
+//   captionText.innerHTML = img.alt;
+// }
+
+
+// function closeModal() {
+//   document.getElementById("yuri-modal").style.display = "none";
+// }
+
+
+
+
