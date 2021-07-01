299fb123ca6f65ccfb7c0f79ca003c7795aeea98
Yuriko Goto <gotoyuriko95@gmail.com>
2021-07-01 08:02:17 +0000

    hai

--- assets/main.js
+++ assets/main.js
@@ -71,3 +71,23 @@ $(function () {
   });
 });
 
+
+//=======================Fade in Animation
+$(function () {
+  $('div').hide().fadeIn(2000);
+});
+
+
+$(function () {
+  $(window).scroll(function () {
+    $('.left-to-right, .down-to-top').each(function () {
+      var elemPos = $(this).offset().top;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll > elemPos - windowHeight + 150) {
+        $(this).addClass('scrollin');
+      }
+    });
+  });
+});
+
